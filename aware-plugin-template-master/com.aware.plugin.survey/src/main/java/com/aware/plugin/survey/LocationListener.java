20d307a22b5dec4e35d9fe682afea9a7fd27e890
Nicholas Blott <blottn@tcd.ie>
2017-03-06 12:17:40 +0000

    added location listener -- WIP

---
+++ aware-plugin-template-master/com.aware.plugin.survey/src/main/java/com/aware/plugin/survey/LocationListener.java
@@ -0,0 +1,34 @@
+package com.aware.plugin.template;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.aware.providers.Locations_Provider;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Nick on 05/03/2017.
+ */
+
+public class LocationListener extends BroadcastReceiver {
+
+    public LocationListener() {
+        super();
+    }
+
+    public void onReceive(Context c, Intent intent) {
+        if (intent == null) {
+            Log.i(TAG, "it was null");
+        }
+        else {
+            if (intent.getExtras() == null) {
+                Log.i(TAG, "hmm the extras were null");
+//                Log.i(TAG, intent.toString());
+            }
+            Log.i(TAG, "Location pinged after null test");
+        }
+    }
+}
