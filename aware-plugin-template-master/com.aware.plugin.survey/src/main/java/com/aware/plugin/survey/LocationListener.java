5d2c2fe2b5ec84b38e7eba38005bf4cc739f5ba5
Nicholas Blott <blottn@tcd.ie>
2017-03-22 17:58:28 +0000

    removed LocationPing class and replaced with cleaner android version, also cleaned up LocationListener

--- aware-plugin-template-master/com.aware.plugin.survey/src/main/java/com/aware/plugin/survey/LocationListener.java
+++ aware-plugin-template-master/com.aware.plugin.survey/src/main/java/com/aware/plugin/survey/LocationListener.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
 import android.util.Log;
 
 import com.aware.providers.Accelerometer_Provider;
@@ -12,7 +13,7 @@ import com.aware.providers.Locations_Provider;
 import static android.content.ContentValues.TAG;
 
 /**
- * Created by Nick on 05/03/2017.
+ * Created by Nicholas on 05/03/2017.
  */
 
 public class LocationListener extends BroadcastReceiver {
@@ -22,23 +23,16 @@ public class LocationListener extends BroadcastReceiver {
     }
 
     public void onReceive(Context c, Intent intent) {
-        Log.i(TAG, "Something was received");
-        if (intent == null) {
-            Log.i(TAG, "it was null");
+        Log.d(TAG, "Received a location broadcast");
+        Cursor data = c.getContentResolver().query(Locations_Provider.Locations_Data.CONTENT_URI, null, null, null, Locations_Provider.Locations_Data.TIMESTAMP + " DESC LIMIT 1");
+        if (data != null && data.moveToFirst()) {
+            Location loc = new Location("Listener");
+            loc.setLatitude(data.getDouble(data.getColumnIndex(Locations_Provider.Locations_Data.LATITUDE)));
+            loc.setLongitude(data.getDouble(data.getColumnIndex(Locations_Provider.Locations_Data.LONGITUDE)));
+            loc.setAccuracy(data.getInt(data.getColumnIndex(Locations_Provider.Locations_Data.ACCURACY)));
+            loc.setTime(data.getLong(data.getColumnIndex(Locations_Provider.Locations_Data.TIMESTAMP)));
+            Log.d(TAG, loc.toString());
         }
-        else {
-            //0 is double_longitude
-            Cursor data = c.getContentResolver().query(Locations_Provider.Locations_Data.CONTENT_URI, null, null, null, Locations_Provider.Locations_Data.TIMESTAMP + " DESC LIMIT 1");
-            if (data != null && data.moveToFirst()) {
-                LocationPing loc = new LocationPing();
-                loc.latitude = data.getDouble(data.getColumnIndex(Locations_Provider.Locations_Data.LATITUDE));
-                loc.longitude = data.getDouble(data.getColumnIndex(Locations_Provider.Locations_Data.LONGITUDE));
-                loc.accuracy = data.getInt(data.getColumnIndex(Locations_Provider.Locations_Data.ACCURACY));
-                loc.timestamp = data.getLong(data.getColumnIndex(Locations_Provider.Locations_Data.TIMESTAMP));
-                Log.i(TAG, loc.toString());
-            }
-            data.close();
-            Log.i(TAG, "Location pinged");
-        }
+        data.close();
     }
 }
